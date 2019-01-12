2cbd2856fdb2460c09fc9fe470e9d846109aee2a
Mary <boch26061984@gmail.com>
2019-01-13 00:58:48 +0500

    Доработка сайта

--- src/index.js
+++ src/index.js
@@ -4,33 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Hello(){
-	let phrase = "World"
-	return (
-		<h1> Hello {phrase}</h1>
-		)
-}
-function SayFullName(props)
-{
-	return(
-	<div>
-	<h1> Мое имя {props.name}, фамилия - {props.surname}</h1>
-	<a href={props.link}>ссылка на мой профиль</a>
-	</div>
-	)
-}
-function MeetAll()
-{
-	return(
-		<div>
-		<SayFullName name="Mary" surname="Fedorova" link="vk.com"/>
-		<SayFullName name="Serj" surname="Fedorova" link="facebook.com"/>
-		<SayFullName name="Helen" surname="Fedorova" link="#"/>
-		</div>
-	)
-}
 
-ReactDOM.render(<MeetAll />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
